8e5919da0668e89193e0153ad817259252f57ca4
Akanksha <nehetea@mcmaster.ca>
2023-01-30 12:09:41 -0500

    created anomaly classes

---
+++ driftgen/__pycache__/anomaly.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import signal
+
+
+class Anomaly:
+    def __init__(self) -> None:
+        pass
+
+
+class RandSeqAnomaly:
+    def __init__(self) -> None:
+        pass
+
+
+class PointAnomaly:
+    def __init__(self) -> None:
+        pass
+
+
+class NoiseAnomaly:
+    def __init__(self) -> None:
+        pass
+
+
+class SegmentScaleAnomaly:
+    def __init__(self) -> None:
+        pass
